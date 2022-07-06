e868ae4f3e9ee0078eafa67284698dc0e871ea52
Ajayachathru <ajayachathru@gmail.com>
2022-07-06 12:49:10 +0530

    Unused contents removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,8 @@ interface Name {
     <div>
       <cdk-virtual-scroll-viewport itemSize="10" class="demo-infinite-container">
         <nz-list nzGrid>
-          <div nz-row [nzGutter]="16">
-            <nz-list-item nz-col [nzSpan]="6" *cdkVirtualFor="let item of ds">
+          <div nz-row [nzGutter]="24">
+            <nz-list-item nz-col [nzSpan]="12" *cdkVirtualFor="let item of ds">
                 <nz-card style="width:300px;" [nzTitle]="item.label">
                   <p>Card content</p>
                   <p>Card content</p>
@@ -61,98 +61,15 @@ interface Name {
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NzDemoListInfiniteLoadComponent implements OnInit, OnDestroy {
+export class NzDemoListInfiniteLoadComponent {
   ds: Array<{
     label: string;
   }> = [];
 
-  private destroy$ = new Subject();
-  constructor(private http: HttpClient, private nzMessage: NzMessageService) {
+  constructor() {
     // this.ds = new MyDataSource(this.http);
-    this.ds = Array.from({ length: 100000 }, (i, v) => ({
-      label: `Card ${v + 1}`,
+    this.ds = Array.from({ length: 100000 }, (v, i) => ({
+      label: `Card ${i + 1}`,
     }));
-    console.log('this.ds', this.ds);
-  }
-
-  ngOnInit(): void {
-    // this.ds
-    //   .completed()
-    //   .pipe(takeUntil(this.destroy$))
-    //   .subscribe(() => {
-    //     this.nzMessage.warning('Infinite List loaded all');
-    //   });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-}
-
-class MyDataSource extends DataSource<ItemData> {
-  private pageSize = 10;
-  private cachedData: ItemData[] = [];
-  private fetchedPages = new Set<number>();
-  private dataStream = new BehaviorSubject<ItemData[]>(this.cachedData);
-  private complete$ = new Subject<void>();
-  private disconnect$ = new Subject<void>();
-
-  constructor(private http: HttpClient) {
-    super();
-  }
-
-  completed(): Observable<void> {
-    return this.complete$.asObservable();
-  }
-
-  connect(collectionViewer: CollectionViewer): Observable<ItemData[]> {
-    this.setup(collectionViewer);
-    return this.dataStream;
-  }
-
-  disconnect(): void {
-    this.disconnect$.next();
-    this.disconnect$.complete();
-  }
-
-  private setup(collectionViewer: CollectionViewer): void {
-    this.fetchPage(0);
-    collectionViewer.viewChange
-      .pipe(takeUntil(this.complete$), takeUntil(this.disconnect$))
-      .subscribe((range) => {
-        if (this.cachedData.length >= 50) {
-          this.complete$.next();
-          this.complete$.complete();
-        } else {
-          const endPage = this.getPageForIndex(range.end);
-          this.fetchPage(endPage + 1);
-        }
-      });
-  }
-
-  private getPageForIndex(index: number): number {
-    return Math.floor(index / this.pageSize);
-  }
-
-  private fetchPage(page: number): void {
-    if (this.fetchedPages.has(page)) {
-      return;
-    }
-    this.fetchedPages.add(page);
-
-    this.http
-      .get<{ results: ItemData[] }>(
-        `https://randomuser.me/api/?results=${this.pageSize}&inc=name,gender,email,nat&noinfo`
-      )
-      .pipe(catchError(() => of({ results: [] })))
-      .subscribe((res) => {
-        this.cachedData.splice(
-          page * this.pageSize,
-          this.pageSize,
-          ...res.results
-        );
-        this.dataStream.next(this.cachedData);
-      });
   }
 }
